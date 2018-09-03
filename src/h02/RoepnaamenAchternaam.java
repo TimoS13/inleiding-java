2d822e395448235445e2aa6dd9e09504eee25aec
timos <timo.slond@student.rocvf.nl>
2018-09-03 11:46:48 +0200

    H02 2.1 commit

--- src/h02/RoepnaamenAchternaam.java
+++ src/h02/RoepnaamenAchternaam.java
@@ -1,4 +1,18 @@
 package h02;
 
-public class RoepnaamenAchternaam {
+import java.awt.*;
+import java.applet.*;
+
+public class RoepnaamenAchternaam extends Applet {
+
+    public void init() {
+        setBackground(Color.WHITE);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.drawString("Timo", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Slond", 50, 70 );
+    }
 }
