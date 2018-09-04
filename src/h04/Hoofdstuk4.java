a889ca003e6764485483289aab8864bb9029846e
timos <timo.slond@student.rocvf.nl>
2018-09-04 09:13:13 +0200

    Hoofdstuk 4 opdracht 2

--- src/h04/Hoofdstuk4.java
+++ src/h04/Hoofdstuk4.java
@@ -1,21 +1,19 @@
 package h04;
 
 import java.applet.Applet;
-
-public class Hoofdstuk4 {
-}
 import java.awt.*;
-import java.applet.*;
 
-public class Hoofdstuk extends Applet {
+public class Hoofdstuk4 extends Applet {
 
     public void init() {
     }
 
     public void paint(Graphics g) {
-        setBackground(Color.blue);
-        g.setColor(Color.yellow);
-        g.drawRect(20, 20, 100, 50);
-        g.fillRect(20, 80, 100, 50);
+        setBackground(Color.white);
+        g.setColor(Color.black);
+        g.drawLine(200, 10, 250,200);
+        g.drawLine(200, 10, 150, 200);
+        g.drawLine(150, 200, 250, 200);
+
     }
 }
