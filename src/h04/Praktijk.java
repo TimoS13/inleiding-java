da40917e58f8803a43624077cb9ad96ea4bec8c8
timos <timo.slond@student.rocvf.nl>
2018-09-05 10:00:36 +0200

    Hoofdstuk 4

--- src/h04/Praktijk.java
+++ src/h04/Praktijk.java
@@ -15,7 +15,14 @@ public class Praktijk extends Applet {
         g.drawLine(10,10,200,10);
         g.drawString("Lijn",90,25);
 
-        
+        g.setColor(Color.BLACK);
+        g.drawRect(20,30,175,75);
+        g.drawString("Rechtroek",80,116);
+
+        g.setColor(Color.BLACK);
+        g.drawRoundRect(20,117,175,75,25,25);
+
+
 
     }
 }
