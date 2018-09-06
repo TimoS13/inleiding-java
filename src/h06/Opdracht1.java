79f560944311ee84d2b3d562069cdde479d0cb5d
timos <timo.slond@student.rocvf.nl>
2018-09-06 10:25:56 +0200

    Hoofdstuk 6

--- src/h06/Opdracht1.java
+++ src/h06/Opdracht1.java
@@ -5,9 +5,9 @@ import java.applet.*;
 
 
 public class Opdracht1 extends Applet {
-    int a;
-    int b;
-    int uitkomst;
+    double a;
+    double b;
+    double uitkomst;
 
     public void init() {
         a = 113;
@@ -18,17 +18,14 @@ public class Opdracht1 extends Applet {
 
     public void paint(Graphics g) {
         //Jan
-        g.drawString("Jan Krijgt: " + uitkomst, 20, 20);
-        g.drawString("Euro",92,20);
+        g.drawString("Jan Krijgt: " + uitkomst+  " euro", 20, 20);
 
-        g.drawString("Ali Krijgt: " + uitkomst, 20, 40);
-        g.drawString("Euro",92,40);
+        g.drawString("Ali Krijgt: " + uitkomst + " euro", 20, 40);
 
-        g.drawString("Jeanette Krijgt: " + uitkomst, 20, 60);
-        g.drawString("Euro",120,60);
+        g.drawString("Jeanette Krijgt: " + uitkomst + " euro", 20, 60);
 
-        g.drawString("Timo Krijgt: " + uitkomst, 20, 80);
-        g.drawString("Euro",100,80);
+        g.drawString("Timo Krijgt: " + uitkomst + " euro", 20, 80);
+
 
 
 
