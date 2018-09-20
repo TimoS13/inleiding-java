21856d5d825ce5d904ab1ca6730c8408a1e2d7d3
timos <timo.slond@student.rocvf.nl>
2018-09-20 14:35:06 +0200

    Hoofdstuk 10

---
+++ src/H10/Opdracht1.java
@@ -0,0 +1,53 @@
+package H10;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Opdracht1 extends Applet {
+    int leeftijd;
+    int nleeftijd;
+    TextField tekstvak;
+    Label label;
+    String tekst;
+    String s;
+
+
+
+    public void init() {
+        tekstvak = new TextField("", 5);
+        add(tekstvak);
+        tekstvak.addActionListener(new tekstvakListener());
+        tekst = "";
+        label = new Label("Geef uw leeftijd in en druk op enter");
+        add(label);
+        add(tekstvak);
+
+        leeftijd = 1;
+
+    }
+    public void paint(Graphics g){
+        g.drawString(s,50,125);
+    }
+
+    class tekstvakListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            s = tekstvak.getText();
+            leeftijd = Integer.parseInt(s);
+           if (nleeftijd < leeftijd){
+                nleeftijd = leeftijd;
+                s = "Uw nieuwe leeftijd is "+ leeftijd;
+                repaint();
+
+            }
+            else {
+                s = "Uw leeftijd is niet groter als de laatste ";
+                repaint();
+           }
+        }
+    }
+}
