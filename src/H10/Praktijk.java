15c19b7524ba4eebf2571787f4fc566b4870b954
timos <timo.slond@student.rocvf.nl>
2018-10-17 13:38:12 +0200

    Hoofdstuk 10 praktijk

--- src/H10/Praktijk.java
+++ src/H10/Praktijk.java
@@ -22,6 +22,7 @@ public class Praktijk extends Applet {
     }
     public void paint (Graphics g){
         g.drawString(tekst,50,100 );
+        g.setColor(Color.red);
 
 
     }
@@ -31,43 +32,29 @@ public class Praktijk extends Applet {
             cijfer = Integer.parseInt(s);
             switch (cijfer){
                 case 1:
-                    getGraphics().setColor(Color.RED);
-                    tekst = "Uw cijfer is Slecht";
-                    break;
                     case 2:
-                    getGraphics().setColor(Color.RED);
-                    tekst = "Uw cijfer is Slecht";
-                    break;
                     case 3:
-                    getGraphics().setColor(Color.RED);
                     tekst = "Uw cijfer is Slecht";
                     break;
                     case 4:
-                    getGraphics().setColor(Color.RED);
                     tekst = "Uw cijfer is Matig";
                     break;
                     case 5:
-                    getGraphics().setColor(Color.ORANGE);
                     tekst = "Uw cijfer is Onvoldoende";
                     break;
                     case 6:
-                    getGraphics().setColor(Color.GREEN);
                     tekst = "Uw cijfer is Voldoende";
                     break;
                     case 7:
-                    getGraphics().setColor(Color.GREEN);
                     tekst = "Uw cijfer is Voldoende";
                     break;
                     case 8:
-                    getGraphics().setColor(Color.GREEN);
                     tekst = "Uw cijfer is Goed";
                     break;
                     case 9:
-                    getGraphics().setColor(Color.GREEN);
                     tekst = "Uw cijfer is Goed";
                     break;
                     case 10:
-                    getGraphics().setColor(Color.GREEN);
                     tekst = "Uw cijfer is Goed";
                     break;
             }
